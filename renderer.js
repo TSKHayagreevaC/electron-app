bf5b7ad17943f6b808c7982462de1fa6d1dc5769
TSKHayagreevaC <srikanth.thiru12@gmail.com>
2023-11-28 19:48:35 +0530

    feat: dark mode toggle.

--- renderer.js
+++ renderer.js
@@ -1,10 +1,9 @@
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${versions.chrome()})`
-
-const func = async () => {
-    const response = await window.versions.ping()
-    console.log(response)
-    alert(response)
-}
-
-func()
+document.getElementById('toggle-dark-mode').addEventListener('click', async () => {
+    const isDarkMode = await window.darkMode.toggle()
+    document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
+  })
+  
+  document.getElementById('reset-to-system').addEventListener('click', async () => {
+    await window.darkMode.system()
+    document.getElementById('theme-source').innerHTML = 'System'
+  })
